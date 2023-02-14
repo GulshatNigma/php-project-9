b7efe2d7fc324db8e3af5239e90a3dc990336d27
Гульшат Нигматуллина <nigma1918@mail.ru>
2023-02-14 22:46:46 +0500

    step 2

--- public/index.php
+++ public/index.php
@@ -5,15 +5,24 @@ require __DIR__ . "/../vendor/autoload.php";
 use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 use Slim\Middleware\MethodOverrideMiddleware;
+use DI\Container;
 
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
+$container = new Container();
 
-$app->get('/', function($request, $responce) {
-    $responce->getBody()->write("Welcome!");
-    return $responce;
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
 
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $responce) {
+    $params = ['url' => ""];
+    return $this->get("renderer")->render($responce, 'main.phtml', $params);
+});
+
+/*$app->post('/urls', function($request, $responce) {
+    return;
+});*/
+
 $app->run();
-
-
