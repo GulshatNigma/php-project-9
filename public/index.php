97300ce803c63be8293497cb42a1e43df2d06314
Гульшат Нигматуллина <nigma1918@mail.ru>
2023-03-28 14:10:01 +0500

    fix linter error

--- public/index.php
+++ public/index.php
@@ -107,7 +107,7 @@ $app->get('/urls', function ($request, $response) {
         $pdo = Connection::get()->connect();
         $result = $pdo->query($sql);
         $dateOfCheck = $result->fetchAll(PDO::FETCH_ASSOC);
-        
+
         if (!empty($dateOfCheck)) {
             $lastChecks[$id] = end($dateOfCheck)['created_at'];
             $lastStatusCode[$id] = end($dateOfCheck)['status_code'];
